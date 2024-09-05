b4269809448bb4762de615e5b4f2cf34cf5ef862
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-05 05:37:31 -0700

    some exercises

--- e-book/src/app/app.component.ts
+++ e-book/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from './body/body.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'e-book';
+  student:string[]=["001","19RP00512","Yvonne IZERIMANA"]
+  imageUrl:string='https://media.istockphoto.com/id/584573082/photo/wedding-hall-or-other-function-facility-set-for-fine-dining.jpg?s=612x612&w=0&k=20&c=Ox9T0JIeGaCXPs_EEoAy5Uc98mY_kCnP9pTz06tb6iI='
+  isDisabled:boolean=true
+  isActive:boolean=true
 }
---
+++ e-book/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'header-root',
+    standalone:true,
+    templateUrl:'./header.component.html',
+    styleUrl:'./header.component.css'
+})
+export class HeaderComponent{
+
+}
