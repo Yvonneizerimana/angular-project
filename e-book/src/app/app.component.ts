41d12a0881d998bb1c17263c7fe33ee3c42017f4
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-19 06:57:52 -0700

    i've done with basic angular

--- e-book/src/app/app.component.ts
+++ e-book/src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { BodyComponent } from './body/body.component';
+import { FormsModule } from '@angular/forms';
+import { NgTemplateOutlet} from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,BodyComponent],
+  imports: [RouterOutlet, HeaderComponent,BodyComponent,FormsModule,NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,6 +20,22 @@ export class AppComponent {
   imageUrl:string='https://media.istockphoto.com/id/584573082/photo/wedding-hall-or-other-function-facility-set-for-fine-dining.jpg?s=612x612&w=0&k=20&c=Ox9T0JIeGaCXPs_EEoAy5Uc98mY_kCnP9pTz06tb6iI='
   isDisabled:boolean=true
   isActive:boolean=true
+  userName:string='JOHN DOE'
+  userOne:string="Umutoniwase Aline"
+
+
+  fullName:string="Musanabera Ange Gaju"
+  isLoggedIn:boolean=false
+  loginCount=0
+
+  names:Array<string>=["Ange","Rugwiro","Umutoni","Vaness","AUdrey"]
+  studentsDetails:Array<any>=[
+    {id:1 ,name:"Yvonne IZERIMANA",Regno:"230100388"},
+    {id:2 ,name:"Ange UMURAZA",Regno:"27Y0388"},
+    {id:3 ,name:"Gimmy KARENZI",Regno:"278760388"},
+    {id:4 ,name:"Berwa MUNYANA",Regno:"76657382"},
+    {id:5 ,name:"Munana Samuel",Regno:"7899100388"},
+  ]
 
   buttonClick(){
     console.log('buttonClicked')
@@ -35,4 +54,30 @@ export class AppComponent {
     this.myName=user.value
 console.log(this.myName)
   }
+
+  updateUsername(username:HTMLInputElement){
+this.userName=username.value
+console.log(this.userName)
+  }
+
+  countNumberOfLogin(){
+    this.loginCount++
+    console.log(this.loginCount)
+  }
+
+  addNewUser(){
+    let newUser={id:6,name:"nuko yeee", RegNumber:"78367834"}
+    
+    this.studentsDetails.push(newUser)
+  }
+  onDelete(){
+     this .studentsDetails.splice(this.studentsDetails.indexOf(this.studentsDetails),1)
+  }
+  constructor(){
+    let length=this.studentsDetails.length
+    console.log(length)
+  }
+
 }
+
+
